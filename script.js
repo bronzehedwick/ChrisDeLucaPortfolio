78de7d36864654ed93f7804f8b736b35ab84cd0f
Chris DeLuca <bronzehedwick@gmail.com>
2012-05-25 22:32:38 -0400

    changed some styling of the nav list

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 "use strict";
 function handle1Clicked() {
     $("#cube").addClass("show-front");
+    console.log("Added show-front");
 }
 $(document).ready( function() {
     $("#1").click(handle1Clicked);
